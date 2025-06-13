15a1ac5b20678da5f3ce6e5866b90bf8de4290fd
Rocio Nava <rocionavaserpa@gmail.com>
2025-06-13 14:14:36 -0300

    Agregando unos edits que quedaron fuera

--- playground_blog/pages/urls.py
+++ playground_blog/pages/urls.py
@@ -5,6 +5,6 @@ urlpatterns = [
     path('', PageListView.as_view(), name='page_list'),
     path('<int:pk>/', PageDetailView.as_view(), name='page_detail'),
     path('create/', PageCreateView.as_view(), name='page_create'),
-    path('<int:pk>/update/', PageUpdateView.as_view(), name='page_update'),
-    path('<int:pk>/delete/', PageDeleteView.as_view(), name='page_delete'),
-]
+    path('<int:pk>/editar/', PageUpdateView.as_view(), name='page_edit'),
+    path('<int:pk>/eliminar/', PageDeleteView.as_view(), name='page_delete'),
+    ]
